1bdff79aed7f941e66e4d1b96bc719648aaa9935
Erick Dewa Pranata <pranatau11012003@gmail.com>
2020-08-08 12:23:11 +0700

    mobile test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+	$('.btn-icon-sidebar').on('click', function(){
+		if(!$('.aro-sidebar').hasClass('active')){
+			$('.aro-sidebar').addClass('active');
+			outside('.aro-sidebar', 'active');
+		}else{
+			$('.aro-sidebar').removeClass('active');
+		}
+	});
+
 	function showSubMenu(el) {
 		if($(el).children('ul').length > 0){
 			$(el).children().first().addClass('active');
@@ -24,4 +33,15 @@ $(document).ready(function(){
 			hideSubMenu(this);
 		}
 	});
+
+	function outside(el, remove){
+		$(document).mouseup(function(e){
+		    var element = $(el);
+		    if(element.hasClass(remove) && $(window).width() <= 980){
+			    if(!element.is(e.target) && element.has(e.target).length === 0){
+			        $(el).removeClass(remove);
+			    }
+		    }
+		});
+	}
 })
